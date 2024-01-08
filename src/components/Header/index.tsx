b4fb1f04ed79af3ed3079f0f60d63d63703ae3e3
calevb <876057433@qq.com>
2024-01-08 18:29:42 +0800

    初始化

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,50 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+
+export default function Footer() {
+    const router = useRouter()
+
+    return (
+        <>
+            <div className="navbar bg-gray-300">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+                        </div>
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                            <li><a onClick={() => router.push('/home')}>首页</a></li>
+                            <li><a onClick={() => router.push('/article')}>文章</a></li>
+                            <li><a onClick={() => router.push('/about')}>关于</a></li>
+                            <li><a onClick={() => router.push('/music')}>音乐</a></li>
+                            <li><a onClick={() => router.push('/todo')}>todo</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="navbar-center">
+                    <a className="btn btn-ghost text-xl">Calebcai's Blog</a>
+                </div>
+                <div className="navbar-end">
+                    <div className="dropdown dropdown-end">
+                        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                            <div className="w-10 rounded-full">
+                                <img alt="Tailwind CSS Navbar component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                            </div>
+                        </div>
+                        {/* <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+              <li>
+                <a className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              <li><a>Settings</a></li>
+              <li><a>Logout</a></li>
+            </ul> */}
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
